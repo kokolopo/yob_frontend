c32b018ccfea2f3d87336f4881c128b0391835dd
herihermwn <herih1876@gmail.com>
2021-12-05 21:22:43 +0700

    Fix: Overflow on open keyboard in login page

--- lib/ui/page/login_page.dart
+++ lib/ui/page/login_page.dart
@@ -19,6 +19,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.transparent,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
